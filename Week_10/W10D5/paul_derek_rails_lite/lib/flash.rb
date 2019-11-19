53661b0e10cb172c67c108fa1126fea205d99839
Derek Shue <dshue@berkeley.edu>
2019-11-18 21:29:29 -0800

    Adding rails lite day 2

--- Week_10/W10D5/paul_derek_rails_lite/lib/flash.rb
+++ Week_10/W10D5/paul_derek_rails_lite/lib/flash.rb
@@ -1,4 +1,29 @@
 require 'json'
 
 class Flash
+    def initialize(req)
+        cookie = req.cookies['_rails_lite_app_flash']
+        @data_now = cookie ? JSON.parse(cookie) : {}
+        @data = {}
+    end
+    
+    def [](key)
+        @data[key.to_s] || @data_now[key.to_s]
+        #@data[cookie]['_rails_lite_app_flash']
+    end
+
+    def []=(key, val)
+        @data[key.to_s] = val
+    end
+
+    def store_flash(res)
+        res.set_cookie(
+            "_rails_lite_app_flash",
+            { path: '/', value: @data.to_json}
+        )
+    end
+
+    def now
+      @data_now
+    end
 end
--- Week_10/W10D5/paul_derek_rails_lite/lib/show_exceptions.rb
+++ Week_10/W10D5/paul_derek_rails_lite/lib/show_exceptions.rb
@@ -1,15 +1,24 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    @app.call(env)
+    #flash[:errors] = @app.errors_full_messages
   end
 
   private
 
   def render_exception(e)
+    begin
+      
+    rescue => exception
+      
+    end
   end
 
 end
