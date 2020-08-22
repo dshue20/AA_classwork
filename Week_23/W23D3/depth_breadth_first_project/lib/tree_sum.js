9b986059c40dd17c2cc46e840727f33f7f212c86
Derek Shue <dshue@berkeley.edu>
2020-08-22 15:09:45 -0700

    Redo tree sum problem

--- Week_23/W23D3/depth_breadth_first_project/lib/tree_sum.js
+++ Week_23/W23D3/depth_breadth_first_project/lib/tree_sum.js
@@ -2,11 +2,11 @@ function treeSum(root) {
     if (!root) return 0;
     let sum = 0;
     let queue = [root];
-    while (queue.length){
-        let node = queue.shift();
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
-        sum += node.val;
+    while (queue.length > 0){
+        let curr = queue.shift();
+        if (curr.left) queue.push(curr.left);
+        if (curr.right) queue.push(curr.right);
+        sum += curr.val;
     };
     return sum;
 }
