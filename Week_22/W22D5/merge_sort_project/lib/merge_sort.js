67f46882d3b5dbcae94016b801eed45380abda53
Derek Shue <dshue@berkeley.edu>
2020-08-21 21:04:44 -0700

    Redo merge sort

--- Week_22/W22D5/merge_sort_project/lib/merge_sort.js
+++ Week_22/W22D5/merge_sort_project/lib/merge_sort.js
@@ -1,26 +1,29 @@
 function merge(array1, array2) {
     let merged = [];
-    while (array1.length != 0 || array2.length != 0){
-        if (array1.length === 0){
-            return merged.concat(array2);
-        } else if (array2.length === 0) {
-            return merged.concat(array1);
-        } else if (array1[0] <= array2[0]) {
-            merged.push(array1.shift())
+    while (array1.length > 0 && array2.length > 0){
+        if (array1[0] < array2[0]){
+            merged.push(array1.shift());
         } else {
-            merged.push(array2.shift())
+            merged.push(array2.shift());
         };
-    }
+    };
+    if (array1.length === 0){
+        merged = merged.concat(array2);
+    } else {
+        merged = merged.concat(array1)
+    };
     return merged;
 }
 
 function mergeSort(array) {
     if (array.length <= 1) return array;
-    let mid = array.length/2;
-    let sortedLeft = mergeSort(array.slice(0,mid));
-    let sortedRight = mergeSort(array.slice(mid));
-    return merge(sortedLeft, sortedRight);
+    const midIdx = Math.floor(array.length/2);
+    const left = mergeSort(array.slice(0,midIdx));
+    const right = mergeSort(array.slice(midIdx));
+    return merge(left, right);
 }
+
+console.log(mergeSort([2, -1, 4, 3, 7, 3]));
 
 module.exports = {
     merge,
