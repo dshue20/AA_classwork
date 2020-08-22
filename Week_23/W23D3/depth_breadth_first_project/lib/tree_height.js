a2096600ecbf529566ffc654ccaf75768a7380f4
Derek Shue <dshue@berkeley.edu>
2020-08-22 14:57:25 -0700

    Redo tree height problem

--- Week_23/W23D3/depth_breadth_first_project/lib/tree_height.js
+++ Week_23/W23D3/depth_breadth_first_project/lib/tree_height.js
@@ -1,21 +1,27 @@
-function treeHeight(root) {
-    if (!root) return -1;
-    let queue = [root];
-    let count = 0;
-    while (queue.length){
-        let node = queue.shift();
-        if (node.left){
-            queue.push(node.left);
-            count++;
-        };
-        if (node.right){
-            queue.push(node.right);
-            if (!node.left) count++;
-        };
-    };
-    return count;
-}
+// function treeHeight(root) {
+//     if (!root) return -1;
+//     let height =0;
+//     if (!root.left && !root.right) return height;
+//     let children = [];
+//     if (root.left) children.push(root.left);
+//     if (root.right) children.push(root.right);
+//     while (children.length > 0){
+//         height += 1;
+//         let length = children.length;
+//         for (let i=0; i < length; i++){
+//             let curr = children.shift();
+//             if (curr.left) children.push(curr.left);
+//             if (curr.right) children.push(curr.right);
+//         };
+//     };
+//     return height;
+// }
 
+function treeHeight(root){
+    if (!root) return -1;
+    if (!root.left && !root.right) return 0;
+    return Math.max(treeHeight(root.left)+1, treeHeight(root.right)+1);
+}
 
 module.exports = {
     treeHeight
