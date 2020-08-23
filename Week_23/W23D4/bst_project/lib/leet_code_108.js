6ba0b0000b3a68cc51b69338e6c995001318a9e2
Derek Shue <dshue@berkeley.edu>
2020-08-22 17:33:35 -0700

    Convert sorted array to bst

--- Week_23/W23D4/bst_project/lib/leet_code_108.js
+++ Week_23/W23D4/bst_project/lib/leet_code_108.js
@@ -10,12 +10,13 @@ class TreeNode {
 }
 
 function sortedArrayToBST(nums) {
-    if (!nums.length) return null;
-    let midIdx = Math.floor(nums.length/2);
-    let root = new TreeNode(nums[midIdx]);
-    root.left = sortedArrayToBST(nums.slice(0,midIdx));
+    if (nums.length === 0) return null;
+    if (nums.length === 1) return new TreeNode(nums[0]);
+    const midIdx = Math.floor(nums.length/2);
+    const root = new TreeNode(nums[midIdx]);
+    root.left = sortedArrayToBST(nums.slice(0, midIdx));
     root.right = sortedArrayToBST(nums.slice(midIdx+1));
     return root;
 }
 
-// [-10,-3,0,5,9,11]
+// [-10,-3,0,5,9,11]z
