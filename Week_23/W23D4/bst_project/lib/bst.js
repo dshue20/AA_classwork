f62b924fea1bf1b6c45c5766955b285f73ab96f7
Derek Shue <dshue@berkeley.edu>
2020-08-22 17:15:32 -0700

    Make bst class

--- Week_23/W23D4/bst_project/lib/bst.js
+++ Week_23/W23D4/bst_project/lib/bst.js
@@ -12,42 +12,56 @@ class BST {
         this.root = null;
     }
 
-    insert(val, root=this.root){
-        let node = new TreeNode(val);
-        if (!root){
+    insert(val){
+        const node = new TreeNode(val);
+        if (!this.root){
             this.root = node;
-        } else if (val < root.val){
-            if (!root.left){
-                root.left = node;
-            } else {
-                this.insert(val, root.left)
-            }
         } else {
-            if (!root.right){
-                root.right = node;
-            } else {
-                this.insert(val, root.right)
+            let curr = this.root;
+            while (curr){
+                if (val < curr.val){
+                    if (!curr.left){
+                        curr.left = node;
+                        curr = null;
+                    } else {
+                        curr = curr.left;
+                    }
+                } else {
+                    if (!curr.right){
+                        curr.right = node;
+                        curr = null;
+                    } else {
+                        curr = curr.right;
+                    }
+                }
             }
         }
-        //console.log(this.root);
     }
 
     searchRecur(val, root=this.root){
-        if (!root) return false;
-        if (root.val === val) return true;
-        return this.searchRecur(val, root.left) || this.searchRecur(val, root.right);
+        if (!root){
+            return false;
+        } else if (root.val === val){
+            return true;
+        } else if (val < root.val){
+            return this.searchRecur(val, root.left)
+        } else {
+            return this.searchRecur(val, root.right)
+        }
     }
 
-    searchIter(val, root=this.root){
+    searchIter(val){
+        if (!this.root) return false;
+        let root = this.root;
         while (root){
             if (root.val === val){
-                return true;
+                return true
             } else if (val < root.val){
                 root = root.left;
             } else {
                 root = root.right;
             }
-        };
+        }
         return false;
     }
 }
