5134297ece83bdd93851c724a925d16560db2058
Derek Shue <dshue@berkeley.edu>
2020-08-24 18:28:07 -0700

    Redo graph bfs

--- Week_23/W23D5/graph_project/lib/breadth_first_search.js
+++ Week_23/W23D5/graph_project/lib/breadth_first_search.js
@@ -1,13 +1,19 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    if (!startingNode) return null;
+    if (startingNode.val === targetVal) return startingNode;
     let visited = new Set();
-    let queue = [startingNode];
-    while (queue.length){
-        node = queue.shift();
-        if (node.val === targetVal) return node;
-        visited.add(node.val);
-        node.neighbors.forEach(neighbor => {
-            if (!visited.has(neighbor.val)) queue.push(neighbor)
-        });
+    visited.add(startingNode);
+    let children = [];
+    for (let child of startingNode.neighbors){
+        children.push(child);
+    }
+    while (children.length > 0){
+        let child = children.shift();
+        if (child.val === targetVal) return child;
+        for (let neighbor of child.neighbors){
+            if (!visited.has(neighbor)) children.push(neighbor);
+        };
+        visited.add(child);
     };
     return null;
 }
