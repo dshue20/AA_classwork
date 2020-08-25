d0ad5f7bafa93ff97ff9825cba0ffefdd7b3feef
Derek Shue <dshue@berkeley.edu>
2020-08-24 18:33:04 -0700

    Redo graph max value

--- Week_23/W23D5/graph_project/lib/max_value.js
+++ Week_23/W23D5/graph_project/lib/max_value.js
@@ -1,13 +1,13 @@
 function maxValue(node, visited=new Set()) {
-    let queue = [node];
-    let max = -Infinity;
-    while (queue.length){
-        checkNode = queue.shift();
-        if (checkNode.val > max) max = checkNode.val;
-        visited.add(checkNode.val);
-        checkNode.neighbors.forEach(neighbor => {
-            if (!visited.has(neighbor.val)) queue.push(neighbor)
-        });
+    let max = node.val;
+    visited.add(node);
+    let unvisited = [];
+    for (let neighbor of node.neighbors) unvisited.push(neighbor);
+    while (unvisited.length > 0){
+        let curr = unvisited.shift();
+        if (curr.val > max) max = curr.val;
+        for (let neighbor of curr.neighbors) if (!visited.has(neighbor)) unvisited.push(neighbor);
+        visited.add(curr);
     };
     return max;
 }
