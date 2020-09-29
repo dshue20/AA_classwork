a491366cef983dff7ace74c3afa4175fd7bcf526
Derek Shue <dshue@berkeley.edu>
2020-09-29 11:47:34 -0700

    Add is bst balanced

--- JSPY/Week_8/W8D2/bst_project/lib/leet_code_110.js
+++ JSPY/Week_8/W8D2/bst_project/lib/leet_code_110.js
@@ -2,6 +2,12 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+var isBalanced = function(root) {
+    if (!root) return true;
+    return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+};
 
+var getHeight = function(root){
+    if (!root) return 0;
+    return Math.max(1 + getHeight(root.left), 1 + getHeight(root.right));
 }
