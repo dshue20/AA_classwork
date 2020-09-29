d4a03dca91dfd4c871004efe0f688401d11a4c33
Derek Shue <dshue@berkeley.edu>
2020-09-29 11:31:05 -0700

    Add sorted array to bst

--- JSPY/Week_8/W8D2/bst_project/lib/leet_code_108.js
+++ JSPY/Week_8/W8D2/bst_project/lib/leet_code_108.js
@@ -2,6 +2,11 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 
-function sortedArrayToBST(nums) {
-
+var sortedArrayToBST = function(nums) {
+    if (!nums.length) return null;
+    const midIdx = Math.floor(nums.length/2);
+    const root = new TreeNode(nums[midIdx]);
+    root.left = sortedArrayToBST(nums.slice(0, midIdx));
+    root.right = sortedArrayToBST(nums.slice(midIdx+1));
+    return root;
 }
