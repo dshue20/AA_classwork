b3c2cb24d9a7938ac96459496593c04e28cd8257
Derek Shue <dshue@berkeley.edu>
2020-09-29 11:30:39 -0700

    Add bst constructor

--- JSPY/Week_8/W8D2/bst_project/lib/bst.js
+++ JSPY/Week_8/W8D2/bst_project/lib/bst.js
@@ -8,7 +8,56 @@ class TreeNode {
 
 
 class BST {
-   
+   constructor(){
+       this.root = null;
+   }
+
+   insert(val, root=this.root){
+       if (!this.root){
+           this.root = new TreeNode(val);
+           return;
+       };
+
+       if (val < root.val){
+           if (!root.left){
+               root.left = new TreeNode(val);
+           } else {
+               this.insert(val, root.left);
+           }
+       } else {
+           if (!root.right){
+               root.right = new TreeNode(val);
+           } else {
+               this.insert(val, root.right);
+           }
+       };
+   }
+
+   searchRecur(val, root=this.root){
+       if (!root){
+            return false;
+       } else if (val === root.val){
+           return true;
+       } else if (val < root.val){
+           return this.searchRecur(val, root.left);
+       } else {
+           return this.searchRecur(val, root.right);
+       };
+   }
+
+   searchIter(val){
+       let root = this.root;
+       while (root){
+           if (val === root.val){
+               return true;
+           } else if (val < root.val){
+               root = root.left;
+           } else {
+               root = root.right;
+           }
+       };
+       return false;
+   }
 }
 
 module.exports = {
