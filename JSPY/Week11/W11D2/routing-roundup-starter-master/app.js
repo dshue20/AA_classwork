49747a641c7936c0a95179e2bf12760320d963a9
Derek Shue <dshue@berkeley.edu>
2021-03-09 16:39:57 -0800

    Add initial pug

---
+++ JSPY/Week11/W11D2/routing-roundup-starter-master/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.all('*', (req, res) => {
+  res.render('phase2', { method: req.method, path: req.path, num: 1 });
+})
+
+const port = 8081;
+app.listen(8081, () => console.log(`App is listening on port ${port}...`));
