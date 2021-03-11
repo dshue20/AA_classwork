49e40583c031b0adf68981dde215400daa056e3a
Derek Shue <dshue@berkeley.edu>
2021-03-11 10:46:15 -0800

    Add error handling

---
+++ JSPY/Week11/W11D5/express-amusement-park-tracker/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan')
+const routes = require('./routes.js');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(routes);
+
+app.use((req, res, next) => {
+  const err = new Error("The requested page could not be found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development'){
+    console.error(err);
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404){
+    res.status(404);
+    res.render('page-not-found', { title: 'Page Not Found' });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.status !== 404){
+    res.status(err.status || 500);
+    let message;
+    let stack;
+    if (process.env.NODE_ENV !== 'production'){
+      message = err.message;
+      stack = err.stack;
+    }
+    res.render('error', { title: 'Server Error', message, stack })
+  }
+});
+
+module.exports = app;
