c2799ac4225b34eb8fe4a343c5717defd89f4eb5
Derek Shue <dshue@berkeley.edu>
2021-03-15 08:54:29 -0700

    Finish sql project

--- JSPY/Week10/W10D1/sql-database-management-starter-master/01-create-users.sql
+++ JSPY/Week10/W10D1/sql-database-management-starter-master/01-create-users.sql
@@ -6,10 +6,13 @@
 -- | aa_times               | 6g73WE2V |
 -- | project_management_app | Q3HphPJ8 |
 
-
+CREATE USER aa_times with PASSWORD '6g73WE2V';
+CREATE USER project_management_app with PASSWORD 'Q3HphPJ8';
 
 
 -- Create the following superuser with the indicated name and password
 -- | name       | password |
 -- |------------|----------|
 -- | data_admin | ik2fiDri |
+
+CREATE USER data_admin with PASSWORD 'ik2fiDri' SUPERUSER;
--- JSPY/Week10/W10D1/sql-database-management-starter-master/02-create-databases.sql
+++ JSPY/Week10/W10D1/sql-database-management-starter-master/02-create-databases.sql
@@ -5,3 +5,6 @@
 -- |-----------------|------------------------|
 -- | aa_times        | aa_times               |
 -- | project_manager | project_management_app |
+
+CREATE DATABASE aa_times with OWNER aa_times;
+CREATE DATABASE project_manager with OWNER project_management_app;
