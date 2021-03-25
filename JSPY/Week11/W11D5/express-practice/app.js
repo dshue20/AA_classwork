f72ea8ba5187fc81c47736b8a9af41299bd4669a
Derek Shue <dshue@berkeley.edu>
2021-03-25 16:49:38 -0700

    Finish practice assessment

--- JSPY/Week11/W11D5/express-practice/app.js
+++ JSPY/Week11/W11D5/express-practice/app.js
@@ -5,11 +5,39 @@
  *       bottom of the file.
  */
 
+const express = require('express');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
 
+const { HairColor, Person } = require('./models');
 
+const csrfProtection = csrf({ cookie: true });
 
+const app = express();
 
+app.set('view engine', 'pug');
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
+
+app.get('/new-person', csrfProtection, async (req, res) => {
+  const hairColors = await HairColor.findAll();
+  res.render('form', { hairColors, csrfToken: req.csrfToken() });
+});
+
+app.post('/new-person', csrfProtection, asyncHandler(async (req, res) => {
+  const { firstName, lastName, age, biography, hairColorId } = req.body;
+  await Person.create({ firstName, lastName, age, biography, hairColorId });
+  res.redirect('/');
+}));
+
+app.get('/', csrfProtection, asyncHandler(async (req, res) => {
+  const people = await Person.findAll({ include: HairColor });
+  res.render('list', { people })
+}));
+
+app.listen(8080, console.log('Listening...'));
 
 /* Do not change this export. The tests depend on it. */
 try {
